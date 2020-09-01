a071d1fd8e4c8286112e693517f27632a6ef7d05
Ivan Pekov <ivan@mrivanplays.com>
2020-09-01 09:00:47 +0300

    Refactor

--- src/main/java/dev/tr7zw/patchcredits/PatchCredits.java
+++ src/main/java/dev/tr7zw/patchcredits/PatchCredits.java
@@ -1,121 +1,129 @@
 package dev.tr7zw.patchcredits;
 
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
+import dev.tr7zw.patchcredits.PatchParser.PatchInfo;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
-
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
-import com.github.mustachejava.DefaultMustacheFactory;
-import com.github.mustachejava.Mustache;
-import com.github.mustachejava.MustacheFactory;
-
-import dev.tr7zw.patchcredits.PatchParser.PatchInfo;
-import lombok.Cleanup;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-
-@Mojo(name = "update", defaultPhase = LifecyclePhase.GENERATE_RESOURCES)
+@Mojo(name = "update", defaultPhase = LifecyclePhase.GENERATE_RESOURCES, threadSafe = true)
 public class PatchCredits extends AbstractMojo {
 
-	/**
-	 * Root directory of the project
-	 */
-	@Parameter(defaultValue = "${project.basedir}", property = "basedir", required = true)
-	private File projectDirectory;
-	
-	/**
-	 * Patch directory
-	 */
-	@Parameter(defaultValue = "${project.basedir}/patches", property = "patchdir", required = true)
-	private File patchDirectory;
-	
-	/**
-	 * Output filename
-	 */
-	@Parameter(defaultValue = "PATCHES.md", property = "outputFile", required = true)
-	private String outputFileName;
-	/**
-	 * src filename
-	 */
-	@Parameter(defaultValue = ".template.md", property = "srcFile", required = true)
-	private String srcFileName;
+  /** Root directory of the project */
+  @Parameter(defaultValue = "${project.basedir}", property = "basedir", required = true)
+  private File projectDirectory;
 
-	public void execute() throws MojoExecutionException {
-		File src = new File(projectDirectory, srcFileName);
-		if (!src.exists()) {
-			super.getLog().warn("Unable to find src at '" + src.getAbsolutePath() + "'! Skipping!");
-			return;
-		}
-		
-		if (!patchDirectory.exists()) {
-			super.getLog().warn("Unable to find patch directory at '" + patchDirectory.getAbsolutePath() + "'! Skipping!");
-			return;
-		}
+  /** Patch directory */
+  @Parameter(defaultValue = "${project.basedir}/patches", property = "patchdir", required = true)
+  private File patchDirectory;
 
-		super.getLog().info("Scanning '" + patchDirectory + "' for patches!");
-		
-		List<PatchInfo> patches = new ArrayList<>();
-		scanFolder(patchDirectory, patches);
-		
-		if(patches.isEmpty()) {
-			super.getLog().warn("Unable to find any patches! Skipping!");
-			return;
-		}
-		
-		patches.sort((a, b) -> a.getSubject().compareTo(b.getSubject()));
-		
-		Output output = new Output();
-		output.setPatches(patches);
-		
-		try {
-			MustacheFactory mf = new DefaultMustacheFactory();
-			@Cleanup
-			FileReader srcReader = new FileReader(src);
-		    Mustache mustache = mf.compile(srcReader, "template");
-		    File outputFile = new File(projectDirectory, outputFileName);
-		    if(outputFile.exists()) {
-		    	outputFile.delete();
-		    }
-		    @Cleanup
-		    OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(outputFile), StandardCharsets.UTF_8);
-		    mustache.execute(writer, output).flush();
-		}catch(IOException ex) {
-			throw new MojoExecutionException("Error while writing the output file!", ex);
-		}
-	}
-	
-	@Getter
-	@NoArgsConstructor
-	@Setter
-	public static class Output{
-		List<PatchInfo> patches;
-	}
-	
-	private void scanFolder(File folder, List<PatchInfo> patches) {
-		for(File f : folder.listFiles()) {
-			if(f.isDirectory()) {
-				scanFolder(f, patches);
-			}else if(f.getName().endsWith(".patch")) {
-				try {
-					patches.add(PatchParser.parsePatch(f));
-				}catch(IOException ex) {
-					super.getLog().warn("Exeption while parseing '" + f.getAbsolutePath() + "'!", ex);
-				}
-			}
-		}
-	}
+  /** Output filename */
+  @Parameter(defaultValue = "PATCHES.md", property = "outputFile", required = true)
+  private String outputFileName;
+  /** src filename */
+  @Parameter(defaultValue = ".template.md", property = "srcFile", required = true)
+  private String srcFileName;
 
+  @Override
+  public void execute() throws MojoExecutionException {
+    File src = new File(projectDirectory, srcFileName);
+    if (!src.exists()) {
+      super.getLog().warn("Unable to find src at '" + src.getAbsolutePath() + "'! Skipping!");
+      return;
+    }
+
+    if (!patchDirectory.exists()) {
+      super.getLog()
+          .warn(
+              "Unable to find patch directory at '"
+                  + patchDirectory.getAbsolutePath()
+                  + "'! Skipping!");
+      return;
+    }
+
+    super.getLog().info("Scanning '" + patchDirectory + "' for patches!");
+
+    List<PatchInfo> patches = new ArrayList<>();
+    scanFolder(patchDirectory, patches);
+
+    if (patches.isEmpty()) {
+      super.getLog().warn("Unable to find any patches! Skipping!");
+      return;
+    }
+
+    patches.sort(Comparator.comparing(PatchInfo::getSubject));
+
+    Output output = new Output();
+    output.setPatches(patches);
+
+    try {
+      MustacheFactory mf = new DefaultMustacheFactory();
+      try (FileReader srcReader = new FileReader(src)) {
+        Mustache mustache = mf.compile(srcReader, "template");
+        File outputFile = new File(projectDirectory, outputFileName);
+        if (outputFile.exists()) {
+          outputFile.delete();
+        }
+        try (OutputStreamWriter writer =
+            new OutputStreamWriter(new FileOutputStream(outputFile), StandardCharsets.UTF_8)) {
+          mustache.execute(writer, output).flush();
+        }
+      }
+    } catch (IOException ex) {
+      throw new MojoExecutionException("Error while writing the output file!", ex);
+    }
+  }
+
+  public static class Output {
+    private List<PatchInfo> patches;
+
+    public Output() {}
+
+    public List<PatchInfo> getPatches() {
+      return patches;
+    }
+
+    public void setPatches(List<PatchInfo> patches) {
+      this.patches = patches;
+    }
+  }
+
+  private void scanFolder(File folder, List<PatchInfo> patches) {
+    File[] files =
+        folder.listFiles(
+            (dir, name) -> {
+              if (dir.isDirectory()) {
+                return !name.equalsIgnoreCase("removed");
+              } else {
+                return true;
+              }
+            });
+    if (files == null) {
+      return;
+    }
+    for (File f : files) {
+      if (f.isDirectory()) {
+        scanFolder(f, patches);
+      } else if (f.getName().endsWith(".patch")) {
+        try {
+          patches.add(PatchParser.parsePatch(f));
+        } catch (IOException ex) {
+          super.getLog().warn("Exeption while parseing '" + f.getAbsolutePath() + "'!", ex);
+        }
+      }
+    }
+  }
 }
--- src/main/java/dev/tr7zw/patchcredits/PatchParser.java
+++ src/main/java/dev/tr7zw/patchcredits/PatchParser.java
@@ -8,59 +8,90 @@ import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.ToString;
+import javax.mail.internet.MimeUtility;
 
 /**
  * Rudimentary parser to get Author, subject and coAuthors of a patch file
- * 
- * @author tr7zw
  *
+ * @author tr7zw
  */
 public class PatchParser {
 
-	public static PatchInfo parsePatch(File file) throws IOException {
-		List<String> lines = Files.readAllLines(file.toPath(), StandardCharsets.UTF_8);
-		String from = "Unknown";
-		String subject = "Unknown";
-		List<String> coAuthors = new ArrayList<>();
-		for(String line : lines) {
-			if(line.startsWith("From: ")) {
-				from = decodeStringIfNeeded(line.replace("From: ", "").split("<")[0].trim());
-			}else if(line.startsWith("Subject: ")) {
-				subject = line.replace("Subject: ", "").replace("[PATCH]", "").trim();
-			}else if(line.startsWith("Co-authored-by: ")) {
-				coAuthors.add(decodeStringIfNeeded(line.replace("Co-authored-by: ", "").split("<")[0].trim()));
-			}
-		}
-		return new PatchInfo(file.getParentFile().getName(), from, subject, coAuthors);
-	}
-	
-	private static String decodeStringIfNeeded(String org) throws IOException{
-		if(org.contains("=") || org.startsWith("=?UTF-8")) {
-			try {
-				return javax.mail.internet.MimeUtility.decodeText(org);
-			}catch(UnsupportedEncodingException ex) {
-				throw new IOException(ex);
-			}
-		}
-		return org;
-	}
-	
-	@Getter
-	@AllArgsConstructor
-	@ToString
-	public static class PatchInfo {
-		private String parent;
-		private String from;
-		private String subject;
-		private List<String> coAuthors;
-		
-		public Function<String, String> getCoAuthorString() {
-			return (s) -> String.join(", ", coAuthors);
-		}
-	}
-	
+  public static PatchInfo parsePatch(File file) throws IOException {
+    List<String> lines = Files.readAllLines(file.toPath(), StandardCharsets.UTF_8);
+    String from = "Unknown";
+    String subject = "Unknown";
+    List<String> coAuthors = new ArrayList<>();
+    for (String line : lines) {
+      if (line.startsWith("From: ")) {
+        from = decodeStringIfNeeded(line.replace("From: ", "").split("<")[0].trim());
+      } else if (line.startsWith("Subject: ")) {
+        subject = line.replace("Subject: ", "").replace("[PATCH]", "").trim();
+      } else if (line.startsWith("Co-authored-by: ")) {
+        coAuthors.add(
+            decodeStringIfNeeded(line.replace("Co-authored-by: ", "").split("<")[0].trim()));
+      }
+    }
+    return new PatchInfo(file.getParentFile().getName(), from, subject, coAuthors);
+  }
+
+  private static String decodeStringIfNeeded(String org) throws IOException {
+    if (org.contains("=") || org.startsWith("=?UTF-8")) {
+      try {
+        return MimeUtility.decodeText(org);
+      } catch (UnsupportedEncodingException ex) {
+        throw new IOException(ex);
+      }
+    }
+    return org;
+  }
+
+  public static class PatchInfo {
+    private final String parent, from, subject;
+    private final List<String> coAuthors;
+
+    public PatchInfo(String parent, String from, String subject, List<String> coAuthors) {
+      this.parent = parent;
+      this.from = from;
+      this.subject = subject;
+      this.coAuthors = coAuthors;
+    }
+
+    public String getParent() {
+      return parent;
+    }
+
+    public String getFrom() {
+      return from;
+    }
+
+    public String getSubject() {
+      return subject;
+    }
+
+    public List<String> getCoAuthors() {
+      return coAuthors;
+    }
+
+    public Function<String, String> getCoAuthorString() {
+      return (s) -> String.join(", ", coAuthors);
+    }
+
+    @Override
+    public String toString() {
+      return "PatchInfo{"
+          + "parent='"
+          + parent
+          + '\''
+          + ", from='"
+          + from
+          + '\''
+          + ", subject='"
+          + subject
+          + '\''
+          + ", coAuthors="
+          + coAuthors
+          + '}';
+    }
+  }
 }
